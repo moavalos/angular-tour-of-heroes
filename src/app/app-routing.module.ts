d39b1da53fafd731fbfd442f94a76b15bd1ba88f
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 16:51:59 -0300

    ruta detalle del heroe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // indican al enrutador que vista mostrar cuando un usuario hace clic en un enlace o pega una url
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
   // coincidir la url con path: heroes y mostrar heroesComponent
 ];
 
