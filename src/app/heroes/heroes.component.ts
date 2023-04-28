1e831944dcd8cd7ac029ea4a9571395d917a5a19
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 12:30:31 -0300

    mostrar heroes simulados

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes', // elementos css para componente
@@ -12,6 +13,8 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };
+  
+  heroes = HEROES;
 
   constructor() { }
 
