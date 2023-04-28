686bbd11db8695da1265a7091d718a023d8e3a20
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 12:06:32 -0300

    mostrar objeto del heroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes', // elementos css para componente
   templateUrl: './heroes.component.html', // ubicacion plantilla componente
   styleUrls: ['./heroes.component.css'] // ubicacion del css privados
 })
-export class HeroesComponent {
+
+export class HeroesComponent { 
+  hero: Hero = { // objeto heroe
+    id: 1,
+    name: 'Windstorm'
+  };
+
   constructor() { }
 
   ngOnInit() {
     // gancho de ciclo de vida. logica de inicializacion.
   }
-
-  hero = 'Windstorm';
-
 }
