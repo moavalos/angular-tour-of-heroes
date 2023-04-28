4310ed583a62937da219a20dbf536634d2121251
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 15:14:55 -0300

    datos del heroe

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+// participante de inyeccion de dependencia.
+// la clase proporciona servicios inyectables y puede tener dependencias.
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,8 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
