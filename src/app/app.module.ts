7fecbb05931b344585af9a196622962f41046c96
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 12:26:10 -0300

    import formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
