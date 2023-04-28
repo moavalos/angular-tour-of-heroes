1c2992a0c9225ece43fb6914a4dfbfcf58491e52
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 16:11:59 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// indican al enrutador que vista mostrar cuando un usuario hace clic en un enlace o pega una url
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
+  // coincidir la url con path: heroes y mostrar heroesComponent
 ];
 
 @NgModule({
