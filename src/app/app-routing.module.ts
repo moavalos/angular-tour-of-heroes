12b1ae0c3071574f7fa4ceb5e94ebd8fe7fbb4d9
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 16:35:33 -0300

    ruta dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // indican al enrutador que vista mostrar cuando un usuario hace clic en un enlace o pega una url
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent }
   // coincidir la url con path: heroes y mostrar heroesComponent
 ];
 
