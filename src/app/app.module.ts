f93737b10347c6bcf006306d77cb1b294791d06e
avalos.m <mora_m_avalos@hotmail.com>
2023-04-28 12:25:07 -0300

    import formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+// archivos y bibliotecas necesita la app = metadata
+// decorador mas importante
 @NgModule({
   declarations: [
     AppComponent,
