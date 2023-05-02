07fc0edc6e9277365a8e9f669911e8538ba48330
avalos.m <mora_m_avalos@hotmail.com>
2023-05-02 12:48:28 -0300

    manejo de errores y handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 // participante de inyeccion de dependencia.
 // la clase proporciona servicios inyectables y puede tener dependencias.
@@ -24,6 +25,11 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+        // catch Error intercepta un observable q falló. pasa el error a un controlador de erores que puede hacer lo q quiera con el error.
+        // handleError informa el error y devuelve un resultado inocuo para q siga funciuonando
+      );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -35,5 +41,26 @@ export class HeroService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+/*Como cada método de servicio devuelve un tipo diferente de resultado 'Observable', handleError() toma un parámetro de tipo para que pueda devolver el valor seguro como el tipo que la aplicación espera.*/
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
