bde07161111d2127a271c559eb9787021eef1b02
avalos.m <mora_m_avalos@hotmail.com>
2023-05-02 13:31:05 -0300

    agregar un heroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,20 +8,30 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'] // ubicacion del css privados
 })
 
-export class HeroesComponent implements OnInit { 
-  
-  heroes?: Hero[];
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { } // inyeccion de dependencia
 
   ngOnInit() {
     // gancho de ciclo de vida. logica de inicializacion.
     this.getHeroes();
-  }  
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes); //pasa el arreglo emitida a la devolución de llamada, que establece la propiedad 'heroes' del componente.
+  }
+
+  // Cuando addHero() se guarda correctamente, la devolución de llamada subscribe() recibe el nuevo héroe y lo empuja a la lista de "héroes" para mostrarlo.
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
 
