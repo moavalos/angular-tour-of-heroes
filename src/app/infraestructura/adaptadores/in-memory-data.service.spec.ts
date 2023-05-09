a1aa33be99c1d985dbc2bc851b0dd0b94f679279
avalos.m <mora_m_avalos@hotmail.com>
2023-05-09 11:58:01 -0300

    import

--- src/app/infraestructura/adaptadores/in-memory-data.service.spec.ts
+++ src/app/infraestructura/adaptadores/in-memory-data.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { InMemoryDataService } from './in-memory-data.service';
 
+// implementacion de interfaz. 
+
 describe('InMemoryDataService', () => {
   let service: InMemoryDataService;
 
